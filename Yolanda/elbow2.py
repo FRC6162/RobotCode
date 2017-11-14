6a74c5963d5c28f1fc2b1e268d1f36c385dc6b4f
YolandaXia <YOLANDA8XIA@gmail.com>
2017-11-14 15:57:00 -0500

    Add file Elbow2

---
+++ Yolanda/elbow2.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 14 15:50:35 2017
+
+@author: Yolanda
+"""
+
+import wpilib
+
+class MyRobot(wpilib.IterativeRobot):
+    
+    def robotInit(self):
+        """
+        This function is called upon program startup and
+        should be used for any initialization code.
+        """
+        self.robot_drive = wpilib.RobotDrive(0,1)
+        self.stick = wpilib.Joystick(1)
+        self.Motor1 = wpilib.VictorSP(4)
+        self.Motor2 = wpilib.VictorSP(5)
+        self.Switch1 = wpilib.DigitalInput(0)
+        self.Switch2 = wpilib.DigitalInput(1)
+        self.Servo1 = wpilib.Servo(6)
+        self.Servo2 = wpilib.Servo(7)
+        
+
+    def autonomousInit(self):
+        """This function is run once each time the robot enters autonomous mode."""
+        self.auto_loop_counter = 0
+
+    def autonomousPeriodic(self):
+        """This function is called periodically during autonomous."""
+
+        # Check if we've completed 100 loops (approximately 2 seconds)
+        if self.auto_loop_counter < 100:
+            self.robot_drive.drive(-0.7, 0) # Drive forwards at half speed
+            self.auto_loop_counter += 1
+        else:
+            self.robot_drive.drive(0, 0)    #Stop robot
+
+    def teleopPeriodic(self):
+        """This function is called periodically during operator control."""
+        #self.robot_drive.arcadeDrive(self.stick)
+        if self.Switch1.get()==True:
+            self.Motor1.set(1)   #This number ranges from -1 to 1-fully reverse to fully forward
+            self.Servo1.set(0.8) #This number ranges from 0 to 1-fully left to fullt right
+        else:
+            self.Motor1.set(0)
+            self.Servo1.set(0.4)
+        if self.Switch2.get()==True:
+            self.Motor2.set(1)
+            self.Servo2.set(0.8)
+        else:
+            self.Motor2.set(0)
+            self.Servo2.set(0.4)
+
+    def testPeriodic(self):
+        """This function is called periodically during test mode."""
+        wpilib.LiveWindow.run()
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
