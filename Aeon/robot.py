763620723867d1bfa8ea0eedbfaf6f3df05937af
wjaneal <william.j.a.neal@gmail.com>
2017-11-14 15:56:46 -0500

    Added some files

--- Aeon/robot.py
+++ Aeon/robot.py
@@ -20,8 +20,8 @@ class MyRobot(wpilib.IterativeRobot):
         This function is called upon program startup and
         should be used for any initialization code.
         """
-        self.robot_drive = wpilib.RobotDrive(0,1)
-        self.stick = wpilib.Joystick(1)
+        self.robot_drive = wpilib.RobotDrive(0,1,2,3)
+        self.stick = wpilib.Joystick(0)
         self.Motor1 = wpilib.VictorSP(4)
         self.Motor2 = wpilib.VictorSP(5)
         self.Switch1 = wpilib.DigitalInput(0)
@@ -46,7 +46,7 @@ class MyRobot(wpilib.IterativeRobot):
 
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
-        #self.robot_drive.arcadeDrive(self.stick)
+        self.robot_drive.arcadeDrive(self.stick)
         if self.stick.getRawButton(1)==True:
             self.Motor1.set(-1)
         if self.stick.getRawButton(2)==True:
@@ -55,6 +55,14 @@ class MyRobot(wpilib.IterativeRobot):
             self.Motor1.set(0)
             #This number ranges from -1 to 1-fully reverse to fully forward
             #self.Servo1.set(0.8) #This number ranges from 0 to 1-fully left to fullt right
+        if self.Switch1.get() == True:
+            if self.stick.getRawButton(3)==True:
+                self.Motor2.set(-1)
+            if self.stick.getRawButton(4)==True:
+                self.Motor2.set(0.5)
+            if self.stick.getRawButton(3)==False and self.stick.getRawButton(4)==False:
+                self.Motor2.set(0)
+                #This number ranges from -1 to 1-fully reverse to fully forward
        
     def testPeriodic(self):
         """This function is called periodically during test mode."""
