5abf8168f06a44f33d1a8c90a133be67a07faca2
zfysoom <zyfsoom@qq.com>
2017-12-04 11:27:56 -0500

    changed robot files

--- Emma/robot.py
+++ Emma/robot.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Oct 10 16:13:16 2017
+
 @author: fy
 """
 
@@ -18,17 +19,17 @@ class MyRobot(wpilib.IterativeRobot):
         This function is called upon program startup and
         should be used for any initialization code.
         """
-        self.robot_drive = wpilib.RobotDrive(0,1)
-        self.robot_drive.setExpiration(0,1)
+        self.robot_drive = wpilib.RobotDrive(0,1,2,3)
         self.stick = wpilib.Joystick(0)
         self.switch1 = wpilib.DigitalInput(0)
         self.switch2 = wpilib.DigitalInput(1)
-        self.switch3 = wpilib.DigitalInput(2)
-        self.switch4 = wpilib.DigitalInput(3)
+        #self.switch3 = wpilib.DigitalInput(2)
+        #self.switch4 = wpilib.DigitalInput(3)
         self.motor1 = wpilib.VictorSP(4)
         self.motor2 = wpilib.VictorSP(5)
         self.Servo1 = wpilib.Servo(6)
         self.Servo2 = wpilib.Servo(7)
+        wpilib.CameraServer.launch()
         
             
     def autonomousInit(self):
@@ -40,7 +41,7 @@ class MyRobot(wpilib.IterativeRobot):
 
         # Check if we've completed 100 loops (approximately 2 seconds)
         if self.auto_loop_counter < 100:
-            self.robot_drive.drive(-0.8, 0) # Drive forwards at half speed
+            self.robot_drive.drive(-0.5, 0) # Drive forwards at half speed
             self.auto_loop_counter += 1
         else:
             self.robot_drive.drive(0, 0)    #Stop robot
@@ -48,28 +49,21 @@ class MyRobot(wpilib.IterativeRobot):
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
         self.robot_drive.arcadeDrive(self.stick)
-        if self.switch1.get()==True:
+        if self.stick.getRawButton(1)==True:
             self.motor1.set(1)
-            self.Servo1.set(self.stick.getRawAxis(2))
-            
+            #self.Servo1.set(self.stick.getRawAxis(2)))
+           
         else:
             self.motor1.set(0)
-            self.Servo1.set(self.stick.getRawAxis(6))
-        
+            #self.Servo1.set(self.stick.getRawAxis(6))
+        '''
         if self.switch2.get()==True:
-            self.motor2.set(self.stick.getRawAxis(1))
+            self.motor2.set(int(self.stick.getRawButton(1)))
             #self.Servo2.set(int(self.stick.getRawButton(2)))
         else:
-            self.motor2.set(self.stick.getRawAxis(5))
+            self.motor2.set(int(self.stick.getRawButton(3)))
             #self.Servo2.set(int(self.stick.getRawButton(4)))
-        if self.stick.getRawButton(1)==True:
-            self.Motor1.set(-1)
-        if self.stick.getRawButton(2)==True:
-            self.Motor1.set(0.5)
-        if self.stick.getRawButton(1)==False and self.stick.getRawButton(2)==False:
-            self.Motor1.set(0)
-            
-
+'''
     def testPeriodic(self):
         """This function is called periodically during test mode."""
         wpilib.LiveWindow.run()
